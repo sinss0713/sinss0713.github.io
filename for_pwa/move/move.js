c1b6a76d353c6dc5ebc36b71d57f8fbacf2d5f46
Sins <sinss0713@gmail.com>
2023-07-17 00:02:18 +0900

    アニメーション追加

--- for_pwa/move/move.js
+++ for_pwa/move/move.js
@@ -1,24 +1,42 @@
 document.addEventListener('DOMContentLoaded',()=>{
     // var count=0;
     var ballx = 10;
-    var bally = 65;
+    var bally = 150;
     var clientWidth;
-    var directionRight = true;
-    const ball=document.getElementById('ball');
+    // let imgWidth;
+    let directionRight = true;
+    const ball = document.getElementById('ball');
+    let imgWidth = ball.clientWidth;
+    let imgDirection = true;
+    let imgNum = 0;
+    let imgStr = 'ball0.png';
+    console.log('imgWidth(2)='+imgWidth);
     // document.addEventListener('click',()=>{ballx = move_ball(ballx,bally)})
 
     function move_ball() {
         clientWidth=document.documentElement.clientWidth;
         let posStr = "";
+        if(imgDirection){
+            imgNum = imgNum+0.1;
+            if(imgNum >= 3){imgDirection = false};
+        } else {
+            imgNum = imgNum-0.1;
+            if(imgNum <= 0){imgDirection = true};
+        }
+        if(imgNum > 4){imgNum = 0};
         if(directionRight){
             ballx += 4;
         } else {
             ballx -= 4;
         }
-        if(ballx > clientWidth - 20){directionRight = false}
+        if(ballx > clientWidth - imgWidth){directionRight = false}
         else if(ballx < 0){directionRight = true};
         posStr = makeStr(ballx,bally);
+        imgStr = 'ball'+Math.round(imgNum)+'.png';
+        // imgStr = 'ball'+imgNum+'.png';
+        console.log('imgStr='+imgStr);
         ball.setAttribute('style',posStr);
+        ball.setAttribute('src',imgStr);
         // console.log("ballx=" + ballx);
         return;
     };
@@ -34,7 +52,7 @@ document.addEventListener('DOMContentLoaded',()=>{
     //     count += 1;
     // };
     // setInterval(関数,ミリ秒); → 関数を引数にとる。
-    setInterval(move_ball,16);
+    setInterval(move_ball,25);
     // setInterval(countTest,1000);
         // if(ballx>500){ballx=100};
         // posStr = makeStr(ballx,bally);
