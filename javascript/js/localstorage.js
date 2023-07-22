c7a64639afef92e1ca3b77b8f0d4cf07f29b5d44
Sins <sinss0713@gmail.com>
2023-07-22 21:46:02 +0900

    localstorage.htmlにステータス欄を追加

--- javascript/js/localstorage.js
+++ javascript/js/localstorage.js
@@ -3,13 +3,25 @@ document.addEventListener('DOMContentLoaded', ()=>{
     const btLoad = document.getElementById("btLoad");
     const result = document.getElementById("result");
     const inputData = document.getElementById("inputData");
+    const status = document.getElementById("status");
     btSave.addEventListener('click', ()=>{
-        let data1 = inputData.value;
-        localStorage.setItem("data1",data1);
+        let data1 = "";
+        data1 = inputData.value;
+        if(data1 == ""){
+            status.textContent="何か入力してください。"
+        }else{
+            localStorage.setItem("data1",data1);
+            status.textContent = '"'+data1+'"をローカルストレージへ保存しました。'
+        }
     });
     btLoad.addEventListener('click', ()=>{
         let resultData1 = localStorage.getItem("data1");
-        result.textContent = resultData1;
+        if(resultData1 == ""){
+            status.textContent = '保存されているデータはありません。';
+        } else {
+            result.textContent = resultData1;
+            status.textContent = '"'+resultData1+'"をローカルストレージから読み込みました。'    
+        };
     });
 
 });
